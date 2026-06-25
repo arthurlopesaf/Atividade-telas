4e895453960c093e2f8e724e6d4890c035724c79
arthurlopesaf <arthurvc132@gmail.com>
2026-06-25 00:20:40 -0300

    Delete projetinho1 directory

--- projetinho1/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module org.example.projetinho1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens org.example.projetinho1 to javafx.fxml;
-    exports org.example.projetinho1;
-}
