f0de34fa5eaca3a2502a0c60e5f68a0f35cffbb3
arthurlopesaf <arthurvc132@gmail.com>
2026-06-25 00:19:52 -0300

    Delete Biblioteca directory

--- Biblioteca/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module org.example.biblioteca {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens org.example.biblioteca to javafx.fxml;
-    exports org.example.biblioteca;
-}
--- Biblioteca/src/main/java/org/example/biblioteca/Launcher.java
+++
@@ -1,9 +0,0 @@
-package org.example.biblioteca;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(BibliotecaApplication.class, args);
-    }
-}
